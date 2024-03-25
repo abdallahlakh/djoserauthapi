b1b6e63f36f692104ba99d2822c13e042279e82f
abdallahlakhdari <a_lakhdari@estin.dz>
2024-03-25 18:25:40 +0100

    2

--- account/urls2.py
+++ account/urls2.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from .views import custom_activation_view,password_rest_view,reset_password_confirm_view
 import account.views as views
 urlpatterns = [
-    path('activate/', custom_activation_view, name='custom_activation'),
-    path('password-reset/', password_rest_view, name='custom_pasword_reset'),
+    path('activate/<str:uid>/<str:token>/', custom_activation_view, name='custom_activation'),
+    path('password-reset/<str:uid>/<str:token>/', password_rest_view, name='custom_pasword_reset'),
     path('reset_password_confirm/', reset_password_confirm_view, name='reset_password_confirm'),
 
    ]
