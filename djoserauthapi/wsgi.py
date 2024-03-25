b9e87cb7ac960c07c4b5cb7ebb11c7ef7ee9efd1
abdallahlakhdari <a_lakhdari@estin.dz>
2024-03-25 11:46:23 +0100

    Your commit message

--- djoserauthapi/wsgi.py
+++ djoserauthapi/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djoserauthapi.settings')
 
 application = get_wsgi_application()
+app=application
