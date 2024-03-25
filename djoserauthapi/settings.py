f483bcfa64d80d73968d0f118c8ee777464a70c7
abdallahlakhdari <a_lakhdari@estin.dz>
2024-03-25 19:11:44 +0100

    3

--- djoserauthapi/settings.py
+++ djoserauthapi/settings.py
@@ -186,11 +186,11 @@ DJOSER = {
 
 
 CORS_ALLOWED_ORIGINS = [
-    "https://lawyer-frontend-alpha.vercel.app/Registration"
+    "https://lawyer-frontend-alpha.vercel.app"
 ]
 
 CORS_ORIGIN_WHITELIST = [
-      "https://lawyer-frontend-alpha.vercel.app/Registration"
+      "https://lawyer-frontend-alpha.vercel.app"
 
 ]
 CORS_ALLOW_CREDENTIALS = True
